cbca5dbe54ff4f819199c0088f7da84165053c5d
Simon Moore <moores@uk.ibm.com>
2019-11-19 13:01:28 +0000

    feat: add SubmittedToCarbon component and deploy it to appropriate pages (#98)

---
+++ src/components/SubmittedToCarbon/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Row, Column } from 'gatsby-theme-carbon/src/components/Grid';
+import { Launch16 } from '@carbon/icons-react';
+import { image, text, textReference } from './SubmittedToCarbon.module.scss';
+import submittedImage from './images/submitted-to-carbon.svg';
+
+const SubmittedToCarbon = ({ to, children }) => {
+  return (
+    <Row>
+      <Column colMd={4} colLg={4}>
+        <img className={image} alt="Submitted to carbon" src={submittedImage} />
+      </Column>
+      <Column colMd={8} colLg={8}>
+        <div className={text}>
+          <div>
+            {children}
+          </div>
+          <div  className={textReference}>
+            Click <a href={to} target="_blank" rel="noopener noreferrer">
+            here <Launch16 fill="currentColor" style={{'vertical-align': 'middle', 'margin': '0 6px 2px 0'}}/></a>
+            to view the Carbon website.
+          </div>
+        </div>
+      </Column>
+    </Row>
+  );
+};
+
+export default SubmittedToCarbon;
--- src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
+++ src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
@@ -1,8 +1,10 @@
 import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
 
 import MdxIcon from '../../../components/MdxIcon';
+import SubmittedToCarbon from '../../../components/SubmittedToCarbon';
 
 export default {
   ...defaultComponents,
   MdxIcon,
+  SubmittedToCarbon,
 };
