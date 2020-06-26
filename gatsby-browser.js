33c4846b3d92bfce284da307148fedd6ee2180bb
Stephane Rodet <RODET@de.ibm.com>
2020-06-26 14:57:29 +0200

    Content redirect of the WalkMe, Getting started and Voice and tone guides (#236)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
-import './src/styles/index.scss';
+require('./src/styles/index.scss');
+
+// Redirect the WalkMe, Getting started and
+// Voice and tone guides to the guidance section
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  let pathName = location.pathname;
+  if (pathName && (
+        pathName.includes('/walkme-guide/') || 
+        pathName.includes('/getting-started-guide/') ||
+        pathName.includes('/voice-and-tone'))) {
+    window.location.href = window.location.href.replace('/pal/', '/guidance/');
+  }
+}
