714652220648f4ab07031b5c7211dc03cabd4e25
syd bailey <syd@syds-MBP.attlocal.net>
2020-09-01 13:39:40 -0500

    adding tbody tags on tables

--- src/components/OneColTable/OneColTable.js
+++ src/components/OneColTable/OneColTable.js
@@ -4,17 +4,19 @@ import { table, cell, cellHeader } from "./OneColTable.module.scss";
 const OneColTable = ({ header, row1, row2 }) => {
   return (
     <table className={table}>
-      <tr>
-        <th className={cell}>
-          <span className={cellHeader}>{header}</span>
-        </th>
-      </tr>
-      <tr>
-        <td className={cell}>{row1}</td>
-      </tr>
-      <tr>
-        <td className={cell}>{row2}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th className={cell}>
+            <span className={cellHeader}>{header}</span>
+          </th>
+        </tr>
+        <tr>
+          <td className={cell}>{row1}</td>
+        </tr>
+        <tr>
+          <td className={cell}>{row2}</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
