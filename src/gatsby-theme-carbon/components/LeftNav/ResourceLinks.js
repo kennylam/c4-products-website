7f2d5aaefb9e4731091072fca766932afd5bc228
syd bailey <syd@syds-MBP.attlocal.net>
2020-09-17 10:32:16 -0500

    feat(left-nav): renoved unnecessary links and replaced some link verbiage

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -3,33 +3,17 @@ import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLi
 
 const links = [
   {
-    title: 'Carbon',
-    href: 'https://www.carbondesignsystem.com/',
-  },
-  {
-    title: 'Our Sketch library',
+    title: 'Sketch library',
     href: 'sketch://add-library/cloud/a831d1a7-486c-4293-af6f-58543496c93a',
   },
   {
-    title: 'Our GitHub repo',
+    title: 'GitHub repo',
     href: 'https://github.ibm.com/CDAI-design/pal',
   },
   {
-    title: 'Our Slack channel',
+    title: 'Slack channel',
     href: 'https://ibm-casdesign.slack.com/archives/CQGR0HC05',
-  },
-  {
-    title: 'IBM Cloud PAL',
-    href: 'https://pages.github.ibm.com/ibmcloud/pal/',
-  },
-  {
-    title: 'IBM Security experience guide',
-    href: 'http://security-design-guide.stage1.mybluemix.net/',
-  },
-  {
-    title: 'IBM Watson experience guide',
-    href: 'https://www.ibm.com/brand/systems/watson/',
-  },
+  }
 ];
 
 // shouldOpenNewTabs: true if outbound links should open in a new tab
