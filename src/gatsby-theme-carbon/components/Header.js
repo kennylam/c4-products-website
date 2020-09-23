05b4a5f586f399ac982a026d53103409b0d160e0
Sydney Bailey <syd@ibm.com>
2020-09-23 09:21:10 -0500

    feat(custom-header): custom header for pal page issue 61 (#324)

---
+++ src/gatsby-theme-carbon/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomHeader = () => {
+  return (<Header>
+    Carbon for&nbsp;<span>Cloud &amp; Cognitive</span>
+  </Header>);
+};
+
+export default CustomHeader;
