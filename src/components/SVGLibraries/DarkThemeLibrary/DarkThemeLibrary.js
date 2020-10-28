5c0aa121e9750fef8e5346f50452753e4c66b447
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-10-28 09:39:45 -0700

    Feat: Add theme prop dark to Pictogram Component.

--- src/components/SVGLibraries/DarkThemeLibrary/DarkThemeLibrary.js
+++ src/components/SVGLibraries/DarkThemeLibrary/DarkThemeLibrary.js
@@ -38,6 +38,7 @@ const DarkThemeLibrary = () => {
       containerIsVisible={containerIsVisible}
       files={files}
       themedResources={allDarkThemeResourcesJson}
+      theme="dark"
     />
   );
 };
