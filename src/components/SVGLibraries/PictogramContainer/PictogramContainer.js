025d64807e03db8309f38190c6db41bbfae79dd1
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-10-28 18:52:41 -0700

    Feat: Remove left and right padding and gave margin top to resource components

--- src/components/SVGLibraries/PictogramContainer/PictogramContainer.js
+++ src/components/SVGLibraries/PictogramContainer/PictogramContainer.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import { Tabs, Tab } from 'gatsby-theme-carbon'
-import LightThemeLibrary from '../LightThemeLibrary/LightThemeLibrary';
-import DarkThemeLibrary from '../DarkThemeLibrary/DarkThemeLibrary';
+import React from "react";
+import { Tabs, Tab } from "gatsby-theme-carbon";
+import LightThemeLibrary from "../LightThemeLibrary/LightThemeLibrary";
+import DarkThemeLibrary from "../DarkThemeLibrary/DarkThemeLibrary";
 
-import './_overrides.scss'
+import "./_overrides.scss";
 
 const PictogramContainer = () => {
   return (
     <Tabs>
-      <Tab label="Light">
+      <Tab label="Light Theme">
         <LightThemeLibrary />
       </Tab>
-      {
-      /* 
+      <span className="light-theme__tab"></span>
+
+      {/*
        * The span is necessary to be able to target the specific element.
-       * Unable to add className to tab itself.  
-       * This is a hack until a better solution can be determined. 
-      */
-      }
-      <span className="dark-theme__tab"></span>
-      <Tab label="Dark">
+       * Unable to add className to tab itself.
+       * This is a hack until a better solution can be determined.
+       */}
+
+      <Tab label="Dark Theme">
         <DarkThemeLibrary />
       </Tab>
-   
     </Tabs>
-
-  )
-}
+  );
+};
 
 export default PictogramContainer;
