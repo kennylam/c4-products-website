5df8c7b3826f0a3abdcc638de55f08e85a7b91b0
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-11-03 14:01:13 -0700

    Fix: search input filter for light theme

--- src/components/SVGLibraries/Pictogram/Pictogram.js
+++ src/components/SVGLibraries/Pictogram/Pictogram.js
@@ -28,6 +28,7 @@ const Pictogram = ({
     <>
       <motion.div variants={searchVariants} initial="hidden" animate="visible">
         <Search
+          data-cypress={`${theme}-illustrations-search`}
           className={pictogramSearch}
           onChange={handleChange}
           labelText="Search SVG Library"
