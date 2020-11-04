036b2748bbd5fec24f4d6217700ed5c4c6edf01e
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-11-04 14:46:47 -0700

    Test: if it really is the download not being found or travis only handling one.

--- cypress/e2e/Pictogram/index.js
+++ cypress/e2e/Pictogram/index.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Test illustrations 'Light' theme library", () => {
+xdescribe("Test illustrations 'Light' theme library", () => {
   it("should navigate to illustrations page from index", () => {
     cy.visit("/");
     cy.get(".bx--header__action--menu").click();
@@ -59,15 +59,15 @@ describe("Test illustrations 'Dark' theme library", () => {
     const button = cy.findByText("Dark Theme").click();
   });
 
-  it('should find the Dark themed "Airport" image exists', () => {
+  xit('should find the Dark themed "Airport" image exists', () => {
     cy.findByAltText(/dark illustration of an airport/i).should("exist");
   });
 
-  it('should check if the Dark themed "Airport" image is visible', () => {
+  xit('should check if the Dark themed "Airport" image is visible', () => {
     cy.findByAltText(/dark illustration of an airport/i).should("be.visible");
   });
 
-  it("should check if all dark images have an alt and src attribute", () => {
+  xit("should check if all dark images have an alt and src attribute", () => {
     cy.findAllByTestId(/dark-img-test/i).each(($el) => {
       cy.wrap($el).should("have.attr", "alt");
       cy.wrap($el).should("have.attr", "src");
@@ -75,7 +75,7 @@ describe("Test illustrations 'Dark' theme library", () => {
   });
 
   it("can download a file", () => {
-    cy.get("[data-cypress=Airport]").click();
+    cy.findAllByTestId(/dark airport/i).click();
 
     const path = cy.task("findFile", "DarkTheme_Airport.svg").then((file) => {
       cy.readFile(file).should("match", /svg/i);
@@ -84,7 +84,7 @@ describe("Test illustrations 'Dark' theme library", () => {
     cy.task("cleanDownloads", "DarkTheme_Airport.svg");
   });
 
-  it('should test "Dark" tab input field and type "truck" ', () => {
+  xit('should test "Dark" tab input field and type "truck" ', () => {
     const input = cy.get("[data-cypress=dark-illustrations-search]");
     input.type("truck", { waitForAnimations: true });
 
@@ -94,7 +94,7 @@ describe("Test illustrations 'Dark' theme library", () => {
       .and("match", /fca87da56b4d370878b742d53f8000b6/);
   });
 
-  it("should check if first image in Pictogram has been removed", () => {
+  xit("should check if first image in Pictogram has been removed", () => {
     cy.findByAltText(/Dark Illustration of an Airport/i).should("not.exist");
   });
 });
