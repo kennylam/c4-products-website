076110c62e87e8f8100ee7e09776e8f881b1688f
Syd <sydabailey@gmail.com>
2021-05-03 13:08:43 -0500

    fix: update branch to master

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
-  pathPrefix: '/cdai-design/pal',
+  pathPrefix: "/cdai-design/pal",
   siteMetadata: {
-    title: 'Carbon for Cloud and Cognitive - Pattern asset library',
-    description: 'Product design library for IBM Cloud, Data and Security',
-    keywords: 'cloud,data,AI,design,pattern,asset,library,pal,security',
+    title: "Carbon for Cloud and Cognitive - Pattern asset library",
+    description: "Product design library for IBM Cloud, Data and Security",
+    keywords: "cloud,data,AI,design,pattern,asset,library,pal,security"
   },
   plugins: [
     // {
@@ -18,41 +17,41 @@ module.exports = {
     //     },
     //   },
     // },
-     {
-      resolve: 'gatsby-remark-images',
+    {
+      resolve: "gatsby-remark-images",
       options: {
-        maxWidth: 1152,
-      },
+        maxWidth: 1152
+      }
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
     {
-      resolve: 'gatsby-theme-carbon',
+      resolve: "gatsby-theme-carbon",
       options: {
         repository: {
-          baseUrl: 'https://github.ibm.com/cdai-design/pal',
-          subDirectory: '/',
-        },
-      },
+          baseUrl: "https://github.ibm.com/cdai-design/pal",
+          branch: "master"
+        }
+      }
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Cloud, Data and AI Design',
-        short_name: 'Cloud, Data and AI Design',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#0062ff',
-        display: 'browser',
+        name: "Cloud, Data and AI Design",
+        short_name: "Cloud, Data and AI Design",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#0062ff",
+        display: "browser",
         icons: []
-      },
+      }
     },
     {
-      resolve: 'gatsby-plugin-matomo',
+      resolve: "gatsby-plugin-matomo",
       options: {
-        siteId: '4',
-        matomoUrl: 'https://hcd-matomo.w3ibm.mybluemix.net',
-        siteUrl: 'https://pages.github.ibm.com/cdai-design/pal',
+        siteId: "4",
+        matomoUrl: "https://hcd-matomo.w3ibm.mybluemix.net",
+        siteUrl: "https://pages.github.ibm.com/cdai-design/pal"
       }
     },
     {
@@ -62,5 +61,5 @@ module.exports = {
         path: `${__dirname}/src/components/SVGLibraries/shared/data`
       }
     }
-  ],
+  ]
 };
