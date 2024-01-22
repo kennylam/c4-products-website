bdc118bdc5651ecedbd533a3bdc6a2b71b4f7849
Tom Waterton <TOMERTON@uk.ibm.com>
2024-01-22 16:43:35 +0000

    Updates contents and links of 404 page (#1494)

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { FourOhFour } from 'gatsby-theme-carbon';
 
 const links = [
-  { href: 'https://pages.github.ibm.com/cdai-design/', text: 'CD&AI Design Home page' },
   { href: '/overview/about', text: 'About this site' },
-  { href: '/components/additional-tag-features', text: 'Components' },
-  { href: '/patterns/advanced-filtering/usage', text: 'Patterns' },
+  { href: '/overview/asset-index', text: 'Asset index' },
+  { href: '/patterns/onboarding/overview', text: 'Onboarding patterns' },
+  { href: `/ai-design/overview`, text: `AI Design guidance`},
+  { href: '/saas-for-hyperscalers/overview', text: 'PLG and MultiCloud SaaS' },
+  { href: '/product-gallery/gallery', text: 'Product gallery' },
   { href: '/content/overview/overview', text: 'Content guide' },
-  { href: '/walkme-guide/overview/overview', text: 'WalkMe guide' },
+  { href: '/design-kits/figma', text: 'Design kits' },
+  { href: '/contributing/overview', text: 'Contributing to IBM Products' },
 ];
 
 const Custom404 = () => <div className={`container-404`}><FourOhFour links={links} /></div>;
