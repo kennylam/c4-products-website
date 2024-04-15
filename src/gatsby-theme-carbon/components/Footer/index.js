61f228f3c9aeafd4a334b236054266cc18a95d8f
kennylam <909118+kennylam@users.noreply.github.com>
2024-04-15 09:27:06 -0400

    chore(editor): remove mdx editor

--- src/gatsby-theme-carbon/components/Footer/index.js
+++ src/gatsby-theme-carbon/components/Footer/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Footer from 'gatsby-theme-carbon/src/components/Footer';
-import packageJson from 'gatsby-theme-carbon/package.json';
-import pageContext from '../../pageContext';
-import cdaiConfig from '../../../../cdai-config.json';
+import React from "react";
+import Footer from "gatsby-theme-carbon/src/components/Footer";
+import packageJson from "gatsby-theme-carbon/package.json";
+// import pageContext from "../../pageContext";
+// import cdaiConfig from "../../../../cdai-config.json";
 
 const currentYear = new Date().getFullYear();
 const versions = {
-  'vanilla-components': packageJson.dependencies["carbon-components"],
-  'react-components': packageJson.dependencies["carbon-components-react"]
+  "vanilla-components": packageJson.dependencies["carbon-components"],
+  "react-components": packageJson.dependencies["carbon-components-react"],
 };
 
 const Content = ({ buildTime }) => (
@@ -15,15 +15,18 @@ const Content = ({ buildTime }) => (
     <p>
       Have questions? Talk to us
       <br />
-      on <a href="https://ibm-casdesign.slack.com/archives/CQGR0HC05">#ibmproducts-pal</a>
+      on{" "}
+      <a href="https://ibm-casdesign.slack.com/archives/CQGR0HC05">
+        #ibmproducts-pal
+      </a>
       <br />
-      or open an issue in <a href="https://github.ibm.com/CDAI-design/pal/issues">GitHub</a>
+      or open an issue in{" "}
+      <a href="https://github.ibm.com/CDAI-design/pal/issues">GitHub</a>
     </p>
-    <p><a href={`https://pages.github.ibm.com/${cdaiConfig.repo}/editor/?page=src/pages${pageContext.relativePagePath}`}>Edit this page</a></p>
     <p>
-      Vanilla Components version {versions['vanilla-components']}
+      Vanilla Components version {versions["vanilla-components"]}
       <br />
-      React Components version {versions['react-components']}
+      React Components version {versions["react-components"]}
       <br />
       Last updated {buildTime}
       <br />
@@ -34,15 +37,15 @@ const Content = ({ buildTime }) => (
 
 const links = {
   firstCol: [
-    { href: '#', linkText: 'Contribute' },
-    { href: 'https://www.ibm.com/privacy', linkText: 'Privacy' },
-    { href: 'https://www.ibm.com/legal', linkText: 'Terms of use' },
-    { href: 'https://www.ibm.com/', linkText: 'IBM.com' },
+    { href: "#", linkText: "Contribute" },
+    { href: "https://www.ibm.com/privacy", linkText: "Privacy" },
+    { href: "https://www.ibm.com/legal", linkText: "Terms of use" },
+    { href: "https://www.ibm.com/", linkText: "IBM.com" },
   ],
   secondCol: [
-    { href: 'https://dribbble.com/_carbondesign/about', linkText: 'Dribble' },
-    { href: 'https://medium.com/carbondesign', linkText: 'Medium' },
-    { href: 'https://twitter.com/_carbondesign', linkText: 'Twitter' },
+    { href: "https://dribbble.com/_carbondesign/about", linkText: "Dribble" },
+    { href: "https://medium.com/carbondesign", linkText: "Medium" },
+    { href: "https://twitter.com/_carbondesign", linkText: "Twitter" },
   ],
 };
 
