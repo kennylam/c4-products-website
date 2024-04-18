46f1496dfb009a20b447da197296d215acc1dae4
kennylam <909118+kennylam@users.noreply.github.com>
2024-04-18 15:41:29 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,57 @@
-exports.onCreateWebpackConfig = ({
-  actions: { setWebpackConfig },
-  plugins: { provide },
-}) => {
-  setWebpackConfig({
-    node: {
-      fs: 'empty',
-    },
-    plugins: [
-      provide({
-        Buffer: ["buffer", "Buffer"],
-        process: "process/browser",
-      }),
-    ],
-    resolve: {
-      fallback: {
-        fs: false,
-        path: require.resolve('path-browserify'),
-        buffer: require.resolve("buffer"),
-      },
-    },
-  });
-};
+const path = require("path");
 
-const { spawn } = require('child_process');
-const getDate = path => new Promise((res) => {
-  const git = spawn('git', ['log', '-1', '--format="%ad"', '--', path]);
-  git.stdout.on("data", data => res(
-    new Date(data.toString().replace(/"/g, ''))
-    .toISOString()));
-  git.on("close", () => res('NOT FOUND'));
-});
+const { spawn } = require("child_process");
+const getDate = (path) =>
+  new Promise((res) => {
+    const git = spawn("git", ["log", "-1", '--format="%ad"', "--", path]);
+    git.stdout.on("data", (data) =>
+      res(new Date(data.toString().replace(/"/g, "")).toISOString())
+    );
+    git.on("close", () => res("NOT FOUND"));
+  });
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === 'Mdx') {
+  if (node.internal.type === "Mdx") {
     const parent = getNode(node.parent);
-    if (parent.internal.type === 'File')
-      getDate(parent.absolutePath).then(date =>
-        createNodeField({ name: 'gitDate', node, value: date, }))
+    if (parent.internal.type === "File")
+      getDate(parent.absolutePath).then((date) =>
+        createNodeField({ name: "gitDate", node, value: date })
+      );
+  }
+};
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  // Allows importing html files for component code examples
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          use: ["html-loader", "markdown-loader"],
+        },
+        {
+          test: /\.html$/,
+          loader: "html-loader",
+          options: {
+            minimize: false,
+          },
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      alias: {
+        // light-weight fork of buble compiler from react-live team
+        buble: path.resolve(__dirname, "node_modules", "@philpl/buble"),
+      },
+    },
+  });
+
+  // Disable sourcemaps in production
+  if (getConfig().mode === "production") {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
   }
 };
