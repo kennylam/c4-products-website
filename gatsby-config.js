c0481176a8e44b38c4c47f0ef4658d8f5982bdad
kennylam <909118+kennylam@users.noreply.github.com>
2024-05-01 13:35:55 -0400

    chore(deps): remove unused deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,14 @@ module.exports = {
   siteMetadata: {
     title: "Carbon for IBM Products",
     description: "Product design library for IBM Cloud, Data and Security",
-    keywords: "cloud,data,AI,design,pattern,asset,library,pal,security"
+    keywords: "cloud,data,AI,design,pattern,asset,library,pal,security",
   },
   plugins: [
     {
       resolve: "gatsby-remark-images",
       options: {
-        maxWidth: 1152
-      }
+        maxWidth: 1152,
+      },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
@@ -20,15 +20,16 @@ module.exports = {
       resolve: "gatsby-theme-carbon",
       options: {
         repository: {
-          baseUrl: "https://github.ibm.com/cdai-design/pal",
-          branch: "main"
+          baseUrl:
+            "https://github.com/carbon-design-system/carbon-for-products-website",
+          branch: "main",
         },
-        iconPath: './src/images/app-icon/light-theme/CarbonforIBMProducts.svg',
+        iconPath: "./src/images/app-icon/light-theme/CarbonforIBMProducts.svg",
         theme: {
-          homepage: 'g10',
-          interior: 'g10',
+          homepage: "g10",
+          interior: "g10",
         },
-      }
+      },
     },
     {
       resolve: "gatsby-plugin-manifest",
@@ -39,15 +40,15 @@ module.exports = {
         background_color: "#ffffff",
         theme_color: "#0062ff",
         display: "browser",
-        icons: []
-      }
+        icons: [],
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/components/SVGLibraries/shared/data`
-      }
-    }
-  ]
+        path: `${__dirname}/src/components/SVGLibraries/shared/data`,
+      },
+    },
+  ],
 };
